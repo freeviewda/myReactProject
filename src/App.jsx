7b3cfeaf8074300ee668d05a7129cd3015f7ca32
jinyong <codebluev@gmail.com>
2025-11-22 13:12:33 +0900

    6.0 introduction

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-// import { useState } from 'react'
-import styles from './App.module.css';
-import Button from './Button.jsx';
+import { useState } from 'react';
 
 function App() {
-
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log('call an api');
   return (
     <div>
-      <h1>Welcome back!!!</h1>
-      <Button className={styles.btn} text="Continue"/>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   )
 }
