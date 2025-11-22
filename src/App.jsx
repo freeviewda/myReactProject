26494eda060451f1d8fc0d36e027064ac3e4618e
jinyong <codebluev@gmail.com>
2025-11-22 12:55:30 +0900

    5.1 Tour of CRA 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 // import { useState } from 'react'
+import styles from './App.module.css';
+import Button from './Button.jsx';
 
 function App() {
 
   return (
     <div>
-      <h1>Welcome back!</h1>
+      <h1>Welcome back!!!</h1>
+      <Button className={styles.btn} text="Continue"/>
     </div>
   )
 }
